16eb2cfcaf7655560e68637e8f9b882640787e5a
jtobal <ziba.tobal@gmail.com>
2024-10-28 20:26:37 -0300

    Se crea la clase habitacion

---
+++ habitacion.ts
@@ -0,0 +1,36 @@
+export abstract class Habitacion {
+    protected numero: number;
+    protected estado: boolean;
+    protected precioBase: number;
+    public constructor(numero: number, estado: boolean, precioBase: number) {
+        this.numero = numero;
+        this.estado = estado;
+        this.precioBase = precioBase;
+    }
+    public abstract reservar(): void;
+    public abstract liberar(): void;
+    public abstract getCostoTotal(): number;
+    public getNumero() {
+        return this.numero;
+    }
+    public setNumero(numero: number) {
+        this.numero = numero;
+    }
+    public getEstado() {
+        return this.estado;
+    }
+    public setEstado(estado: boolean) {
+        this.estado = estado;
+    }
+    public getPrecioBase() {
+        return this.precioBase;
+    }
+    public setPrecioBase(precioBase: number) {
+        this.precioBase = precioBase;
+    }
+
+   
+
+
+
+}
